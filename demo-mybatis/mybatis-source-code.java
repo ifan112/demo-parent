c7dee2de9eaaf7e482bdbd18a3152460c8012945
ifan112 <ifan.w112@gmail.com>
2019-03-03 19:19:42 +0800

    mybatis-基于xml配置文件的Configuration构造器解析

--- demo-mybatis/mybatis-source-code.java
+++ demo-mybatis/mybatis-source-code.java
@@ -7,8 +7,33 @@ SqlSessionFactory sqlSessionFactory = new SqlSessionFactory().build(in) {
     // properties是在创建SqlSessionFactory时，开发者自定义的配置属性。它将会与开发者在配置文件中声明的properties合并
     build(in, String environment = null , Properties properties = null) {
         try {
-            // xml配置文件解析器
-            XMLConfigBuilder configBuilder = new XMLConfigBuilder(in, environment, properties);
+            // 基于xml配置文件的Configuration构造器
+            XMLConfigBuilder configBuilder = new XMLConfigBuilder(in, environment, properties) {
+                // xml配置文件解析器
+                XPathParser parser = new XPathParser(in, true, properties, new XMLMapperEntityResolver());
+                this(parser, envionment, properties) {
+                    // mybatis全部配置
+                    Configuration configuration = new Configuration() {
+                        // 预注册一些内置的类型别名
+                        typeAliasRegistry.registryAlias("JDBC", JdbcTransactionFactory.class);
+                        typeAliasRegistry.registryAlias("POOLED", PooledDataSourceFactory.class);
+
+                        // ...
+                    };
+                    // 开发者编码方式自定义的配置属性。稍后，它将会与配置文件中的properties合并
+                    configuration.setVariables(properties);
+
+                    super(configuration) {
+                        // XMLConfigBuilder持有configuration对象，稍后将会边解析xml配置文件，边更新该configuration
+                        this.configuration = configuration;
+                        this.typeAliasRegistry = configuration.getTypeAliasRegistry();
+                        this.typeHandlerRegistry = configuration.getTypeHandlerRegistry();
+                    };
+                    this.parsed = false;            // 默认尚未解析过配置文件
+                    this.environment = environment; // 指定的环境
+                    this.parser = parser;
+                }
+            };
 
             // 开始解析mybatis配置文件
             configBuilder.parse() {
@@ -67,12 +92,16 @@ SqlSessionFactory sqlSessionFactory = new SqlSessionFactory().build(in) {
   
 /* ------------------------------------------------------------------------------------------------------------------ */
 
-// XMLConfigBuilder解析mybatis配置文件，创建Congiuration对象
-XMLConfigBuilder
-    -- Configuration configuration  // 配置
-    -- String environment           // 当前环境标识
-    -- boolean parsed               // 是否已经解析过配置文件的标识。mybatis配置文件只可以解析一次，重复解析将会抛出异常
-    -- XPathParser parser           // xml配置文件的解析器
+BaseBuilder
+    |   -- Configuration configuration              // 配置
+    |   -- TypeAliasRegistry typeAliasRegistry      // 类型别名映射。从类型别名映射到数据类型类，例如string->java.lang.String
+    |   -- TypeHandlerRegistry typeHandlerRegistry  // 类型处理器映射。从数据类型类映射到该数据类型的处理器，例如String->StringTypeHandler
+    |
+    |
+XMLConfigBuilder    // XMLConfigBuilder解析mybatis配置文件，创建Congiuration对象
+        -- String environment   // 当前环境标识
+        -- boolean parsed       // 是否已经解析过配置文件的标识。mybatis配置文件只可以解析一次，重复解析将会抛出异常
+        -- XPathParser parser   // xml配置文件的解析器
 
 // 持有mybatis所有配置
 Configuration
