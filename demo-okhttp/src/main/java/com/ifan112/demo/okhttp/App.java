969232aaca79b013d63ff342701fd566aa678998
ifan112 <ifan.w112@gmail.com>
2022-01-19 11:05:07 +0800

    okhttp

---
+++ demo-okhttp/src/main/java/com/ifan112/demo/okhttp/App.java
@@ -0,0 +1,15 @@
+package com.ifan112.demo.okhttp;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+public class App {
+    public static void main(String[] args) {
+        // OkHttpClient httpClient = new OkHttpClient();
+        //
+        // Request.Builder builder = new Request.Builder();
+        // builder.method()
+        //
+        // httpClient.newCall()
+    }
+}
